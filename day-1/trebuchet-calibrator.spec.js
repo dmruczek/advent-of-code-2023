84f717cdd2305c12428b293ee81fdb36da7ee44d
dmruczek <dmruczek@gmail.com>
2023-12-07 16:54:05 -0500

    test fix

--- day-1/trebuchet-calibrator.spec.js
+++ day-1/trebuchet-calibrator.spec.js
@@ -5,11 +5,10 @@ describe('TrebuchetCalibrator', function () {
     describe('extractNumbers', function () {
         it('should extract all of the numbers from the given string', function () {
             const trebuchetCalibrator = new TrebuchetCalibrator();
-            expect(trebuchetCalibrator.extractNumbers('1abc2')).toBe([ '1', '2' ]);
-            expect(trebuchetCalibrator.extractNumbers('pqr3stu8vwx')).toBe([ '3', '8' ]);
-            expect(trebuchetCalibrator.extractNumbers('a1b2c3d4e5f')).toBe([ '1', '2', '3', '4', '5' ]);
-            expect(trebuchetCalibrator.extractNumbers('treb7uchet')).toBe([ '7' ]);
-
+            expect(trebuchetCalibrator.extractNumbers('1abc2')).toEqual([ '1', '2' ]);
+            expect(trebuchetCalibrator.extractNumbers('pqr3stu8vwx')).toEqual([ '3', '8' ]);
+            expect(trebuchetCalibrator.extractNumbers('a1b2c3d4e5f')).toEqual([ '1', '2', '3', '4', '5' ]);
+            expect(trebuchetCalibrator.extractNumbers('treb7uchet')).toEqual([ '7' ]);
         });
     });
 
